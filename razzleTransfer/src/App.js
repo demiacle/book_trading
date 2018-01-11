208354cede90167d3704e2575e1ebe1ebee61b34
demiacle <demiacle@gmail.com>
2018-01-11 14:36:50 -0800

    Hooked up server side rendering

--- razzleTransfer/src/App.js
+++ razzleTransfer/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
-import Home from './Home';
+import Home from './home/Home';
+import Books from './books/Books';
+import Profile from './profile/Profile';
 import './App.css';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
+const App = (serverData) =>{
+return <Switch>
+    <Route exact path="/" render={i=><Home {...serverData}/>} />
+    <Route exact path="/profile" render={i=><Profile {...serverData}/>} />
+    <Route exact path="/books" render={i=><Books {...serverData}/>} />
   </Switch>
-);
+};
 
 export default App;
--- razzleTransfer/src/home/Home.js
+++ razzleTransfer/src/home/Home.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import logo from './react.svg';
+import logo from '../react.svg';
 import './Home.css';
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+    console.log('constructor props are ')
+    console.log(props)
+  }
   render() {
+    console.log( 'component has this data')
+    console.log( this.props )
+    console.log( this.state )
     return (
       <div className="Home">
         <div className="Home-header">
           <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
+          <h2>Welcome to HOME PAGE and props are</h2>
         </div>
         <p className="Home-intro">
           To get started, edit <code>src/App.js</code> or{' '}
