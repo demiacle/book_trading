b7acbdfc64c1910060c8c89e6dbdad9f56347723
demiacle <demiacle@gmail.com>
2018-01-11 18:13:08 -0800

    Moved razzle migration to root

---
+++ src/nav/Nav.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="/">
+              <div>home</div >
+            </a>
+          </li>
+          <li>
+            <a href="/books">
+              <div>books</div>
+            </a>
+          </li>
+          <li className="right">
+            <form action="/search">
+              <input type="text" placeholder="Search.." name="search" />
+              <button type="submit">search</button>
+            </form>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
--- src/typescripts/hello.js
+++
@@ -1,5 +0,0 @@
-function ta(person) {
-    return "hellow, " + person;
-}
-var users = 1;
-ta(users);
