216b71eedf356b98ed3eabd34f569138fca75154
demiacle <demiacle@gmail.com>
2018-01-31 12:17:20 -0800

    Changed some build/script settings for heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  modify: (config, {target, dev}, webpack )=>{
-    config.devtool = 'inline-source-map'
+  modify: (config, { target, dev }, webpack) => {
+    config.devtool = "inline-source-map";
     /*
     config.output.devtoolModuleFilenameTemplate = function(info){
       return info.absoluteResourcePath;
     }
     */
-    return config
+    return config;
   }
-}
+};
